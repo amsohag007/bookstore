3a80756486a4468867cca4c390787b1290688d98
amsohag007 <amsohag007@gmail.com>
2023-08-04 17:40:11 +0600

    feat(auth): password reset dtos and services added

--- src/authentications/dtos/verify-password-reset-code.dto.ts
+++ src/authentications/dtos/verify-password-reset-code.dto.ts
@@ -1,18 +1,18 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsDefined,
   IsEmail,
   IsEnum,
   IsNotEmpty,
   IsOptional,
-  IsString
-} from "class-validator";
+  IsString,
+} from 'class-validator';
 
 export class VerifyPasswordResetCodeDTO {
   @ApiProperty({
     type: String,
-    description: "Provide user Email when reseting password using email",
-    required: false
+    description: 'Provide user Email when reseting password using email',
+    required: false,
   })
   @IsString()
   @IsEmail()
@@ -21,28 +21,28 @@ export class VerifyPasswordResetCodeDTO {
 
   @ApiProperty({
     type: String,
-    description: "Provide user phone when reseting password using phone no",
-    required: false
+    description: 'Provide user phone when reseting password using phone no',
+    required: false,
   })
   @IsString()
   @IsOptional()
-  phone: string;
+  phone: string = null;
 
   @ApiProperty({
     type: String,
     description:
-      "Type of method (EMAIL or PHONE) for requesting forgot password",
-    required: false
+      'Type of method (EMAIL or PHONE) for requesting forgot password',
+    required: false,
   })
   @IsDefined()
   @IsString()
-  @IsEnum(["EMAIL", "PHONE"])
-  type: string;
+  @IsEnum(['EMAIL', 'PHONE'])
+  type: string = 'EMAIL';
 
   @ApiProperty({
     type: String,
-    description: "Provide user phone when reseting password using phone no",
-    required: false
+    description: 'Provide user phone when reseting password using phone no',
+    required: false,
   })
   @IsDefined()
   @IsString()
